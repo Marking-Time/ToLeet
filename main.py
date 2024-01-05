75659a2903f3ef7b6f5e925322f17f55ec760d36
Marking-Time <153015560+Marking-Time@users.noreply.github.com>
2024-01-05 18:56:38 +0000

    Add: Code for toRoman - update readme

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 leet = {"a": "4", "b": "13", "c":"[", "d":"[)", "e":"3", "f":"|=","g": "6", "h":"#", "i": "|","j":".]", "k":"|<", "l":"1","m":"|y|", "n":"|\|", "o":"0", "p":"|>", "q":"o,","r":"I2", "s":"5", "t":"7", "u":"[_]", "v":"-v", "w":"|v|", "x":"}{", "y":"'/","z":"2", "1":"i", "2":"ii", "3":"iii", "4":"iv", "5":"v", "6":"vi", "7":"vii", "8":"viii", "9":"ix","0":".", " ":" ", "-":"3", ".":"3", ",":"3"}
 
-input_string = "WASHINGTON  Sen. Dianne Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was 90"
+input_string = "9WASHINGTON  Sen. Dianne Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was 90 "
 
 def to_leet(string):
   string = str(string)
@@ -12,9 +12,28 @@ def to_leet(string):
     letter = leet[letter]
     new_string += letter
   return new_string
-print(to_leet(input_string))
 
-#def to_roman(number):
+print(input_string)
+print(to_leet(input_string))
+print()
+
+def to_roman(number):
+  number1 = []
+  text = []
+  for letter in input_string:
+    letter = int(letter)
+    if isinstance(letter, str):
+      text.append(letter)
+      # print(text)
+    if isinstance(letter, int):
+      print(True)
+      number1.append(letter)
+      print(number1)
+
+  return number1
+
+print(to_roman(input_string))
+      
 
 
   
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 ### To Do:
 - Roman Numeral Conversion - The existing code only converts the digits  and not into the roman numeral syntax
 - Consider making this into an object based program with multiple output methods -  like into other character systems
+- Worked on code to convert roman numerals into arabic
+  - string to int conversions may require try except logic
+  - Code doesn't work properly - exits with traceback
