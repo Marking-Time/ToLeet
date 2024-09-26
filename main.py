f9460d4e9ef8e6604923cf43f3d3e83fd1fa349d
Mark  King <g34519838@gmail.com>
2024-09-26 12:07:08 -0700

    Changes to ssupport new IDE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Takes a lower case string, numbers pass through to convert roman
 from typing import final
-import regex as re
+import re as re
 
 
 leet = {"a": "4", "b": "13", "c":"[", "d":"[)", "e":"3", "f":"|=","g": "6", "h":"#", "i": "|","j":".]", "k":"|<", "l":"1","m":"|y|", "n":"|\|", "o":"0", "p":"|>", "q":"o,","r":"I2", "s":"5", "t":"7", "u":"[_]", "v":"-v", "w":"|v|", "x":"}{", "y":"'/","z":"2", "1":"i", "2":"ii", "3":"iii", "4":"iv", "5":"v", "6":"vi", "7":"vii", "8":"viii", "9":"ix","0":".", " ":" ", "-":"3", ".":"3", ",":"3"}
